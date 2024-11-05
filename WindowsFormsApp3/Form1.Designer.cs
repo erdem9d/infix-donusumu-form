98331370f11b8212050e65b3d34fe8840b73a886
erdem9d <omer.erdem124@gmail.com>
2024-11-05 20:50:10 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(372, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 95);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dönüştür";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(372, 230);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 30);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(198, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "postfix sonucu";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(646, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "prefix sonuç";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 537);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static int Precedence(char op)
+        {
+            if (op == '+' || op == '-') return 1;
+            if (op == '*' || op == '/') return 2;
+            if (op == '^') return 3;
+            return 0; // Parantezler veya geçersiz operatörler için öncelik sıfır.
+        }
+
+        // Karakterin operand (değişken veya sayı) olup olmadığını kontrol eder.
+
+        static bool IsOperand(char ch)
+        {
+            return char.IsLetterOrDigit(ch); // Eğer karakter harf veya rakam ise, true döndürür.
+
+        }
+
+        // Infix ifadeyi Postfix'e dönüştüren fonksiyon. 
+        static string infixdenpostfixe(string infix)
+        {
+            Stack<char> stack = new Stack<char>(); // Operatörleri tutmak için bir yığın(stack) oluşturuyoruz.
+           
+            string postfix = ""; // Postfix sonucu tutmak için bir string. 
+
+            // İfade içerisindeki her karakteri kontrol ediyoruz. 
+            foreach (char ch in infix)
+            {
+                if (IsOperand(ch))
+                    postfix += ch; // Operand (sayı veya değişken) ise postfix stringine eklenir. 
+                
+                else if (ch == '(')
+                    stack.Push(ch); // Sol parantez ise yığına eklenir. 
+                else if (ch == ')')
+                {
+                    // Sağ parantez ise yığın açılana kadar operatörler postfix stringine eklenir. 
+                    
+                    while (stack.Count > 0 && stack.Peek() != '(')
+                        postfix += stack.Pop();
+                    stack.Pop(); // Sol parantez yığından çıkarılır. 
+                }
+                else
+                {
+                    // Operatörler yığına eklenmeden önce öncelik sırasına göre postfix stringine eklenir.
+                    
+                    while (stack.Count > 0 && Precedence(stack.Peek()) >= Precedence(ch))                   
+                    postfix += stack.Pop();
+                    stack.Push(ch); // Yeni operatör yığına eklenir. 
+                }
+            }
+
+            // Yığındaki kalan operatörler postfix stringine eklenir. 
+            while (stack.Count > 0)
+                postfix += stack.Pop();
+
+            return postfix; // Postfix ifadesini döndürürüz. 
+        }
+
+        // Infix ifadeyi Prefix'e dönüştüren fonksiyon. 
+        static string infixdenprefixe(string infix)
+        {
+            // Infix ifadeyi ters çeviriyoruz. 
+            char[] chars = infix.ToCharArray();
+            Array.Reverse(chars);
+            infix = new string(chars);
+
+            // Parantezleri ters çeviriyoruz: '(' yerine ')' ve ')' yerine '('  yapıyoruz.
+           
+                        infix = infix.Replace('(', '#').Replace(')', '(').Replace('#', ')');
+
+            // Ters çevrilen infix'i postfix'e dönüştürürüz.
+            string postfix = infixdenpostfixe(infix);
+
+            // Postfix ifadesini ters çeviriyoruz. 
+            chars = postfix.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars); // Prefix ifadesini döndürürüz. 
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string infix = textBox1.Text; // Kullanıcının girdiği infix ifadeyi alır.
+
+
+            // Infix ifadeyi hem postfix hem de prefix ifadeye dönüştürür.
+            string postfix = infixdenpostfixe(infix);
+            string prefix = infixdenprefixe(infix);
+
+            // Elde edilen sonuçları etiketlerde gösterir.
+            label1.Text = "Postfix: " + postfix;
+            label2.Text = "Prefix: " + prefix;
+
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
